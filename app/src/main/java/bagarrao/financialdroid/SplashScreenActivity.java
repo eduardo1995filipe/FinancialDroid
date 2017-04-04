87a6fcd7ac08a7df05ec5b6583903dcebbf3d2ec
Eduardo Bagarrão <eduardo_95@live.com.pt>
2017-04-05 00:40:33 +0100

    - Added SplashScreen; - Added Main Menu Activity.

---
+++ app/src/main/java/bagarrao/financialdroid/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package bagarrao.financialdroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
