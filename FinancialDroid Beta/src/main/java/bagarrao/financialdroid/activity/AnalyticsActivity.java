ea5a3e1dad1ef1b379435c140d335b4b39eec3dd
eduardo1995filipe <eduardo1995filipe@users.noreply.github.com>
2017-05-01 18:14:12 +0100

    - minor fixes

--- FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/AnalyticsActivity.java
+++ FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/AnalyticsActivity.java
@@ -2,25 +2,34 @@ package bagarrao.financialdroid.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 
-import bagarrao.financialdroid.R;
+import java.util.LinkedList;
 
 public class AnalyticsActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
-    private ListView[] monthList;
-    private ImageButton[] chartByMonthButton;
+
+    private LinkedList<Integer> yearList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_analytics);
     }
 
+    /**
+     *
+     */
     public void init() {
+        this.linearLayout = new LinearLayout(this);
+        this.linearLayout.setOrientation(LinearLayout.VERTICAL);
+        this.yearList = new LinkedList<>();
+    }
 
+    /**
+     *
+     */
+    public void setupLayout() {
+        setContentView(linearLayout);
     }
 }
