9d89ed71eab9a322d6d6f437f174595f2f28c59d
eduardo1995filipe <eduardo1995filipe@users.noreply.github.com>
2017-05-01 11:51:11 +0100

    - code refactor, reformat and identation.  - Analytics development

--- FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/AnalyticsActivity.java
+++ FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/AnalyticsActivity.java
@@ -2,18 +2,25 @@ package bagarrao.financialdroid.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import bagarrao.financialdroid.R;
 
 public class AnalyticsActivity extends AppCompatActivity {
 
+    private LinearLayout linearLayout;
+    private ListView[] monthList;
+    private ImageButton[] chartByMonthButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
+    public void init() {
+
+    }
 }
