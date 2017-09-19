fca2c9807f706a90467745bf418e8bea48c3f198
Eduardo Bagarrao <eduardo_95@yahoo.com>
2017-09-20 00:41:13 +0100

    - AnalyticsActivity is now functional! It's in a early development phase.

--- FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/AnalyticsActivity.java
+++ FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/AnalyticsActivity.java
@@ -10,10 +10,17 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import bagarrao.financialdroid.R;
+import bagarrao.financialdroid.database.ExpenseDataSource;
+import bagarrao.financialdroid.expense.Expense;
+import bagarrao.financialdroid.expense.ExpenseType;
+import bagarrao.financialdroid.utils.Filter;
 
 public class AnalyticsActivity extends AppCompatActivity {
+
+    private PieDataSet dataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,45 +30,54 @@ public class AnalyticsActivity extends AppCompatActivity {
 
         PieChart pieChart = (PieChart) findViewById(R.id.lastMonthPieChart);
 
-        ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(4f, 0));
-        entries.add(new Entry(8f, 1));
-        entries.add(new Entry(6f, 2));
-        entries.add(new Entry(12f, 3));
-        entries.add(new Entry(18f, 4));
-        entries.add(new Entry(9f, 5));
-
-        PieDataSet dataset = new PieDataSet(entries, "# of Calls");
+        setAmountsByType();
 
         ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
+        labels.add("FEEDING");
+        labels.add("TRANSPORTS");
+        labels.add("SCHOOL");
+        labels.add("CLOTHING");
+        labels.add("OTHERS");
 
         PieData data = new PieData(labels, dataset);
-        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
-        pieChart.setDescription("Description");
+        dataset.setColors(ColorTemplate.PASTEL_COLORS);
+        pieChart.setDescription("");
         pieChart.setData(data);
 
         pieChart.animateY(5000);
 
-        pieChart.saveToGallery("/sd/mychart.jpg", 85); // 85 is the quality of the image
+//        pieChart.saveToGallery("/sd/mychart.jpg", 85); // 85 is the quality of the image
 
 
 
 
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        setAmountsByType();
     }
+
+    public float getExpensesAmountByType(ExpenseType type){
+        ExpenseDataSource dataSource = new ExpenseDataSource(this);
+        dataSource.open();
+        List<Expense> expenses = Filter.getExpensesByType(dataSource.getAllExpenses(),type);
+        double amount = 0;
+        for(Expense e : expenses)
+            amount += e.getValue();
+        return (float)amount;
+    }
+
+    public void setAmountsByType(){
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(getExpensesAmountByType(ExpenseType.FEEDING), 0));
+        entries.add(new Entry(getExpensesAmountByType(ExpenseType.TRANSPORTS), 1));
+        entries.add(new Entry(getExpensesAmountByType(ExpenseType.SCHOOL), 2));
+        entries.add(new Entry(getExpensesAmountByType(ExpenseType.CLOTHING), 3));
+        entries.add(new Entry(getExpensesAmountByType(ExpenseType.OTHERS), 4));
+
+        dataset = new PieDataSet(entries, "Expense Types");
+    }
+
 }
