260ccea150216e54027086a6cfe26c1c546fb6d9
eduardo1995filipe <eduardo_95@yahoo.com>
2018-01-06 12:34:40 +0000

    - InterstitialAd will only appear if the has has internet connection;
    - Interstitial ad intent turned to a global variable;
    
    - Removed backup call;
    
    - Documented class.

--- FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/SplashScreenActivity.java
+++ FinancialDroid Beta/src/main/java/bagarrao/financialdroid/activity/SplashScreenActivity.java
@@ -1,44 +1,78 @@
 package bagarrao.financialdroid.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import bagarrao.financialdroid.backup.Backup;
 import bagarrao.financialdroid.currency.CurrencyConverter;
-import bagarrao.financialdroid.migration.Migrator;
 
 /**
+ * Simple splash screen activity that shows
+ * the logo and the name of the application.
+ *
  * @author Eduardo Bagarrao
  */
 public class SplashScreenActivity extends AppCompatActivity {
 
+    /**
+     * Probability of {@link InterstitialAdActivity} appear instead
+     * of {@link LoginActivity}. This is also dependent from
+     * the available internet connection.
+     */
+    private static final double AD_PROBABILITY = 0.35;
+
+    /**
+     * {@link CurrencyConverter} singleton instance.
+     */
     private CurrencyConverter currencyConverter = CurrencyConverter.getInstance();
 
-	private static final double AD_PROBABILITY = 0.4;
+    /**
+     * {@link Intent} that will launch {@link LoginActivity}.
+     */
+    private Intent loginIntent;
 
-    private Intent intent;
+    /**
+     * {@link Intent} that will launch {@link InterstitialAdActivity}.
+     */
+    private Intent interstitialAdIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        currencyConverter.init(this);
-        new Backup(this).go(); //for now its not needed
-        this.intent = new Intent(this, LoginActivity.class);
+//        new Backup(this).go();
+        this.loginIntent = new Intent(this, LoginActivity.class);
+        this.interstitialAdIntent = new Intent(this, InterstitialAdActivity.class);
+        this.currencyConverter.init(this);
         setup();
         finish();
     }
 
     /**
-     * setup of the class objects
+     * setup of the {@link SplashScreenActivity} objects.
+     * Checks if the with a random number if {@link InterstitialAdActivity}
+     * will appear or if it goes to {@link LoginActivity} without showing the ad Activity.
      */
     public void setup(){
-        if (Migrator.needsMigration(this)) {
-            new Migrator(this).run();
-        }
-        if(Math.random() < AD_PROBABILITY)
-            startActivity(new Intent(this, InterstitialAdActivity.class));
+//        if (Migrator.needsMigration(this)) { isto ja nao vai ser feito aqui
+//            new Migrator(this).run();
+//        }
+        if(Math.random() < AD_PROBABILITY && isNetworkConnected())
+            startActivity(interstitialAdIntent);
         else
-            startActivity(intent);
+            startActivity(loginIntent);
+    }
+
+    /**
+     * Checks whether if exists a internet connection available or not.
+     * This will decide whether if the {@link InterstitialAdActivity} will
+     * appear or not.
+     *
+     * @return boolean
+     */
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null;
     }
 }
